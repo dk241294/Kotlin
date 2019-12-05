a0986c996cccb28c09763ef5d110a8c722e101eb
deepak kumar <deepak24121994@gmail.com>
2019-12-05 22:07:02 +0530

    largest of two number

---
+++ LargestOfTwo/src/largest.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var num1: Int = 22
+    var num2: Int = 33
+    var result: Int = 0
+   result= if (num1 > num2) {
+         num1
+    } else {
+         num2
+    }
+
+    println(result)
+}
