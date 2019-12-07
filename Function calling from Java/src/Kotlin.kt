d72e68f17987ee219d799d7119ad230665bf6888
deepak kumar <deepak24121994@gmail.com>
2019-12-07 14:55:51 +0530

    Function calling from java

---
+++ Function calling from Java/src/Kotlin.kt
@@ -0,0 +1,11 @@
+@file:JvmName("First")// anotation use here to change class name
+
+fun main(){
+ var result=add(2,3)
+    println(result)
+
+}
+fun add(a:Int,b:Int):Int{
+    return a+b
+
+}
---
+++ Function calling from Java/src/java.java
@@ -0,0 +1,7 @@
+public class java {
+    public static void main(String[] args) {
+        //   int result = KotlinKt.add(2, 3);
+        int result = First.add(2, 3);
+        System.out.println(result);
+    }
+}
